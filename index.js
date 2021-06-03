133b7943d0f62fb0b7294fd24a62acf1d52949d4
SinecKers <yasar.ylmz.yt@gmail.com>
2021-06-03 23:40:15 +0300

    upload photo

--- index.js
+++ index.js
@@ -3,14 +3,30 @@ const path = require("path");
 const ejs = require("ejs");
 const dotenv = require('dotenv');
 dotenv.config();
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+
+mongoose.connect(process.env.MONGOURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Photo = require('./models/Photo');
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(fileUpload());
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -20,6 +36,37 @@ app.get("/about", (req, res) => {
 app.get("/add", (req, res) => {
   res.render("add");
 });
+
+app.post("/photos", async (req, res) => {
+  console.log(req.files)
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
+
+  /*await Photo.create(req.body)
+  res.redirect('/')*/
+})
+
+app.get("/photos/:id", async (req, res) => {
+  const photo = await Photo.findById(req.params.id)
+  res.render('photo', {
+    photo
+  })
+})
+
 
 const port = 3000;
 app.listen(port, () => {
