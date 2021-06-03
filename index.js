b6df9427ffd824c94ebad2181b76e09bfa8f429c
SinecKers <yasar.ylmz.yt@gmail.com>
2021-06-04 00:39:30 +0300

    heroku deploy

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ dotenv.config();
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
+const methodOverride = require('method-override');
+const photoController = require('./controllers/photoController');
+const pageController = require('./controllers/pageController');
+
 
 mongoose.connect(process.env.MONGOURL, {
   useNewUrlParser: true,
@@ -21,52 +25,20 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(fileUpload());
-
-app.get("/", async (req, res) => {
-  const photos = await Photo.find({})
-  res.render('index', {
-    photos
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/add", (req, res) => {
-  res.render("add");
-});
-
-app.post("/photos", async (req, res) => {
-  console.log(req.files)
-  const uploadDir = 'public/uploads';
-
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-
-  let uploadeImage = req.files.image;
-  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
-
-  uploadeImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: '/uploads/' + uploadeImage.name,
-    });
-    res.redirect('/');
-  });
-
-  /*await Photo.create(req.body)
-  res.redirect('/')*/
-})
-
-app.get("/photos/:id", async (req, res) => {
-  const photo = await Photo.findById(req.params.id)
-  res.render('photo', {
-    photo
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
   })
-})
+);
 
+app.get('/', photoController.getAllPhotos);
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
 const port = 3000;
 app.listen(port, () => {
